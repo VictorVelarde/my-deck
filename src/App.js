e4f682c990cc40b0a9151ce7968cc777f85a12ef
Víctor Velarde <victor.velarde@gmail.com>
2017-07-19 12:35:45 +0200

    Added a simple LineLayer from DeckGL

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react';
 import MapGL from 'react-map-gl';
-// https://github.com/zjhch123/react-map-gl-demo-with-create-react-app
-// import mapboxgl from 'mapbox-gl'; //https://github.com/uber/react-map-gl/issues/176
-//require('script!mapbox-gl/dist/mapbox-gl.js');
+import DeckGL, { LineLayer } from 'deck.gl';
 
 const token =
     'pk.eyJ1IjoidnZlbGFyZGUiLCJhIjoiY2lzMW10aW5rMDA3aDJ5bzdvN25jaXFwYiJ9.gWQR2LQ1vXnuPx2bH_zq2g'; //process.env.MAPBOX_ACCESS_TOKEN; // eslint-disable-line
+
+const viewport = {
+    width: 1000,
+    height: 500,
+    longitude: -3.776577,
+    latitude: 43.443417,
+    zoom: 11,
+    pitch: 0,
+    bearing: 0
+};
+
+// Data to be used by the LineLayer
+const data = [
+    {
+        sourcePosition: [-3.776577, 43.443417],
+        targetPosition: [-3.676577, 43.443417]
+    }
+];
+
 class App extends Component {
     render() {
         return (
             <MapGL
-                width={1000}
-                height={500}
-                latitude={43.443417}
-                longitude={-3.776577}
-                zoom={11}
+                {...viewport}
                 mapStyle="mapbox://styles/mapbox/dark-v9"
                 mapboxApiAccessToken={token}
-            />
+            >
+                <DeckGL
+                    {...viewport}
+                    layers={[
+                        new LineLayer({
+                            id: 'line-layer',
+                            data
+                        })
+                    ]}
+                />
+            </MapGL>
         );
     }
 }
